49c3f82c23c8ff94def653cab148808dc0b49010
LucasSkool <lyu1@hwemail.com>
2026-01-21 11:48:53 -0800

    1/21/26 Work - "After Day 5 Methods" completed, no tester for them yet

--- Unit 4 Exercises/Unit4Exercises.java
+++ Unit 4 Exercises/Unit4Exercises.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 
 public class Unit4Exercises {
-
     // Method 1: matchingEndSequences
     public static boolean matchingEndSequences(int[] nums, int n) {
         for (int i = 1; i <= n; i++, n--) {
-            if (nums[i-1] != nums[nums.length-n]) {
+            if (nums[i - 1] != nums[nums.length - n]) {
                 return false;
             }
         }
@@ -18,7 +17,7 @@ public class Unit4Exercises {
     public static boolean hasThreeConsecutive(int[] nums) {
         //odd + even at same time
         for (int i = 0; i < nums.length - 2; i++) {
-            if ( (nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i] % 2 == 1)  //odd
+            if ((nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i] % 2 == 1)  //odd
                 || (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i] % 2 == 0)) {  //even
                 return true;
             }
@@ -31,7 +30,7 @@ public class Unit4Exercises {
     public static int[] generateNumberSequence(int start, int end) {
         int[] newArr = new int[end - start];
         for (int i = start; i < end; i++) {
-            newArr[i-start] = i;
+            newArr[i - start] = i;
         }
         return newArr;
     }
@@ -40,7 +39,7 @@ public class Unit4Exercises {
     public static String[] fizzBuzz(int start, int end) {
         String [] newArr = new String[end - start];
         for (int i = start; i < end; i++) {
-            newArr[i-start] = String.valueOf(i);
+            newArr[i - start] = String.valueOf(i);
         }
 
         for (int i = 0; i < newArr.length; i++) {
@@ -80,27 +79,47 @@ public class Unit4Exercises {
 
     // Method 1: noNegatives
     public static ArrayList<Integer> noNegatives(ArrayList<Integer> nums) {
-        // to-do: implement the method
-        return new ArrayList<>();
+        ArrayList<Integer> newArrList = new ArrayList<Integer>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) >= 0) {
+                newArrList.add(nums.get(i));
+            }
+        }
+        return newArrList;
 
     }
 
     // Method 2: excludeTeenNumbers
     public static ArrayList<Integer> excludeTeenNumbers(ArrayList<Integer> nums) {
-        // to-do: implement the method
-        return new ArrayList<>();
+        ArrayList<Integer> newArrList = new ArrayList<Integer>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) < 13 && nums.get(i) > 19) {
+                newArrList.add(nums.get(i));
+            }
+        }
+        return newArrList;
     }
 
     // Method 3: appendY
     public static ArrayList<String> appendY(ArrayList<String> strs) {
-        // to-do: implement the method
-        return new ArrayList<>();
+        ArrayList<String> newStrList = new ArrayList<String>();
+        for (int i = 0; i < strs.size(); i++) {  //goes through input ArrayList
+            if (Character.toLowerCase(strs.get(i).charAt(strs.get(i).length() - 1)) != 'y') {
+                newStrList.add(strs.get(i) + "y");
+            }
+        }
+        return newStrList;
     }
 
     // Method 4: squarePlus10
     public static ArrayList<Integer> squarePlus10(ArrayList<Integer> nums) {
-        // to-do: implement the method
-        return new ArrayList<>();
+        for (int i = 0; i < nums.size(); i++) {
+            nums.set(i, ((nums.get(i) * nums.get(i)) + 10));
+            if (nums.get(i) % 10 == 5 || nums.get(i) % 10 == 6) {
+                nums.remove(i);
+            }
+        }
+        return nums;
     }
 
 }
