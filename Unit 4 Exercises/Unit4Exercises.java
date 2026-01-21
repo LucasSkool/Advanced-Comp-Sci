2725b6dae9b7a20a29cc5c6d08d44abaef017b8e
LucasSkool <lyu1@hwemail.com>
2026-01-20 19:34:34 -0800

    1/20/25 Work - Finished Unit 4 Array List Exercises (first 5 methods)

---
+++ Unit 4 Exercises/Unit4Exercises.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+
+public class Unit4Exercises {
+
+    // Method 1: matchingEndSequences
+    public static boolean matchingEndSequences(int[] nums, int n) {
+        for (int i = 1; i <= n; i++, n--) {
+            if (nums[i-1] != nums[nums.length-n]) {
+                return false;
+            }
+        }
+        return true;
+        //I know this one seems very strange because I had to go back because
+        //I misunderstood the question but it works
+    }
+
+    // Method 2: hasThreeConsecutive
+    public static boolean hasThreeConsecutive(int[] nums) {
+        //odd + even at same time
+        for (int i = 0; i < nums.length - 2; i++) {
+            if ( (nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i] % 2 == 1)  //odd
+                || (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i] % 2 == 0)) {  //even
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Method 3: generateNumberSequence
+    public static int[] generateNumberSequence(int start, int end) {
+        int[] newArr = new int[end - start];
+        for (int i = start; i < end; i++) {
+            newArr[i-start] = i;
+        }
+        return newArr;
+    }
+
+    // Method 4: fizzBuzz
+    public static String[] fizzBuzz(int start, int end) {
+        String [] newArr = new String[end - start];
+        for (int i = start; i < end; i++) {
+            newArr[i-start] = String.valueOf(i);
+        }
+
+        for (int i = 0; i < newArr.length; i++) {
+            if (Integer.parseInt(newArr[i]) % 3 == 0 && Integer.parseInt(newArr[i]) % 5 == 0) {
+                newArr[i] = "FizzBuzz";
+            } else if (Integer.parseInt(newArr[i]) % 3 == 0) {
+                newArr[i] = "Fizz";
+            } else if (Integer.parseInt(newArr[i]) % 5 == 0) {
+                newArr[i] = "Buzz";
+            }
+            
+        } //there's probably a more concise way but idk..
+        return newArr;
+    }
+
+    // Method 5: moveEvenBeforeOdd
+    public static int[] moveEvenBeforeOdd(int[] nums) {
+        int[] newArr = new int[nums.length];
+        int newArrIndex = 0;
+        for (int i = 0; i < nums.length; i++) {  //even
+            if (nums[i] % 2 == 0) {
+                newArr[newArrIndex] = nums[i];
+                newArrIndex++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {  //and THEN odd
+            if (nums[i] % 2 == 1) {
+                newArr[newArrIndex] = nums[i];
+                newArrIndex++;
+            }
+        }
+
+        return newArr;
+    }
+
+    // ArrayList Methods
+
+    // Method 1: noNegatives
+    public static ArrayList<Integer> noNegatives(ArrayList<Integer> nums) {
+        // to-do: implement the method
+        return new ArrayList<>();
+
+    }
+
+    // Method 2: excludeTeenNumbers
+    public static ArrayList<Integer> excludeTeenNumbers(ArrayList<Integer> nums) {
+        // to-do: implement the method
+        return new ArrayList<>();
+    }
+
+    // Method 3: appendY
+    public static ArrayList<String> appendY(ArrayList<String> strs) {
+        // to-do: implement the method
+        return new ArrayList<>();
+    }
+
+    // Method 4: squarePlus10
+    public static ArrayList<Integer> squarePlus10(ArrayList<Integer> nums) {
+        // to-do: implement the method
+        return new ArrayList<>();
+    }
+
+}
---
+++ Unit 4 Exercises/Unit4ExercisesTester.java
@@ -0,0 +1,84 @@
+public class Unit4ExercisesTester {
+    public static void main (String[] args) {
+        
+        //all of these tests are just the example ones in the instruction page except the last one in fizzBuzz
+        
+        System.out.println();
+        System.out.println("\n----------------- METHOD 1 TESTS -----------------\n");
+        int[] nums = new int[] {5, 6, 45, 99, 13, 5, 6};
+        System.out.println("Array:" + toString(nums));
+        System.out.println("Matching End Sequence for n = 1? : " + Unit4Exercises.matchingEndSequences(nums, 1));
+
+        System.out.println("Matching End Sequence for n = 2? : " + Unit4Exercises.matchingEndSequences(nums, 2));
+
+        System.out.println("Matching End Sequence for n = 3? : " + Unit4Exercises.matchingEndSequences(nums, 3));
+
+        System.out.println("\n\n----------------- METHOD 2 TESTS -----------------\n");
+
+        int[] nums2 = new int[] {2, 1, 3, 5};
+        System.out.println("Array:" + toString(nums2));
+        System.out.println("3 Consecutive? : " + Unit4Exercises.hasThreeConsecutive(nums2));
+
+        int[] nums3 = new int[] {2, 1, 2, 5};
+        System.out.println("Array:" + toString(nums3));
+        System.out.println("3 Consecutive? : " + Unit4Exercises.hasThreeConsecutive(nums3));
+
+        int[] nums4 = new int[] {2, 4, 2, 5};
+        System.out.println("Array:" + toString(nums4));
+        System.out.println("3 Consecutive? : " + Unit4Exercises.hasThreeConsecutive(nums4));
+
+        System.out.println("\n\n----------------- METHOD 3 TESTS -----------------\n");
+
+        System.out.println("Num Seq. (5, 10) : " + toString(Unit4Exercises.generateNumberSequence(5, 10)));
+
+        System.out.println("Num Seq. (11, 18) : " + toString(Unit4Exercises.generateNumberSequence(11, 18)));
+
+        System.out.println("Num Seq. (1, 3) : " + toString(Unit4Exercises.generateNumberSequence(1, 3)));
+
+        System.out.println("\n\n----------------- METHOD 4 TESTS -----------------\n");
+
+        System.out.println("FizzBuzz. (1, 6) : " + toString(Unit4Exercises.fizzBuzz(1, 6)));
+
+        System.out.println("FizzBuzz. (1, 8) : " + toString(Unit4Exercises.fizzBuzz(1, 8)));
+
+        System.out.println("FizzBuzz. (1, 16) : " + toString(Unit4Exercises.fizzBuzz(1, 16)));
+        //not exactly same as instruction examples to get "FizzBuzz" (11 --> 16)
+
+        System.out.println("\n\n----------------- METHOD 5 TESTS -----------------\n");
+        
+        int[] nums5 = new int[] {1, 0, 1, 0, 0, 3, 1};
+        System.out.println("Array:" + toString(nums5));
+        System.out.println("3 Consecutive? : " + toString(Unit4Exercises.moveEvenBeforeOdd(nums5)));
+
+        int[] nums6 = new int[] {3, 3, 2};
+        System.out.println("Array:" + toString(nums6));
+        System.out.println("3 Consecutive? : " + toString(Unit4Exercises.moveEvenBeforeOdd(nums6)));
+
+        int[] nums7 = new int[] {2, 2, 2};
+        System.out.println("Array:" + toString(nums7));
+        System.out.println("3 Consecutive? : " + toString(Unit4Exercises.moveEvenBeforeOdd(nums7)));
+
+        
+    }
+
+    //i coded these for my tester
+    //i feel like usually they don't have parameters but i think i forgot how to do that so whatever they work anyways
+    public static String toString(int[] nums) {
+        String result = "[";
+        for (int i = 0; i < nums.length; i++) {
+            result += nums[i] + ", ";
+        }
+        result = result.substring(0, result.length() - 2) + "]";
+        return result;
+    }
+
+    public static String toString(String[] strings) {
+        String result = "[";
+        for (int i = 0; i < strings.length; i++) {
+            result += strings[i] + ", ";
+        }
+        result = result.substring(0, result.length() - 2) + "]";
+        return result;
+    }
+
+}
